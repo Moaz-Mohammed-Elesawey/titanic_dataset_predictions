83da648ef7621e42e95050aabb8e2da7c465d6af
Moaz-Mohammed-Elesawey <62155531+Moaz-Mohammed-Elesawey@users.noreply.github.com>
2020-04-11 01:16:16 -0700

    Add files via upload

--- titanic_competition.py
+++ titanic_competition.py
@@ -4,11 +4,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier  #, AdaBoostClassifier
-from tqdm import tqdm
+from sklearn import svm
 # from sklearn.tree import DecisionTreeClassifier
 # from sklearn.neighbors import KNeighborsClassifier
 # from sklearn.gaussian_process import GaussianProcessClassifier
 # from sklearn.metrics import accuracy_score
+
 
 def change_sex(x):
     if x == 'male':
@@ -26,8 +27,7 @@ def change_Embark(x):
         return 2
 
 
-
-def get_train_data_ready(file_name='train.csv'):
+def get_train_data_ready(file_name='data/train.csv'):
     data = pd.read_csv(file_name).copy()
     data.set_index('PassengerId', inplace=True)
     data.fillna(0, inplace=True)
@@ -39,6 +39,7 @@ def get_train_data_ready(file_name='train.csv'):
     survived = np.array(data.pop('Survived'))
     data = np.array(data)
     return data, survived
+
 X, Y = get_train_data_ready()
 
 
@@ -46,7 +47,7 @@ X, Y = get_train_data_ready()
 X = np.nan_to_num(X)
 Y = np.nan_to_num(Y)
 
-def get_test_data_ready(file_name='test.csv'):
+def get_test_data_ready(file_name='data/test.csv'):
     data = pd.read_csv(file_name).copy()
     data.set_index('PassengerId', inplace=True)
     data.fillna(0, inplace=True)
@@ -57,19 +58,28 @@ def get_test_data_ready(file_name='test.csv'):
     data['Embarked'] = data['Embarked'].apply(change_Embark)
     data = np.array(data)
     return data
+
 test_data = get_test_data_ready()
 
 print(X.shape, Y.shape, test_data.shape)
 
 
 #######  RandomForestClassifier  ########
+RF_clf = RandomForestClassifier(n_estimators=10, max_depth=6, n_jobs=1, verbose=0)
+RF_clf.fit(X, Y)
+RF_predictions = RF_clf.predict(test_data)
 
-clf = RandomForestClassifier(n_estimators=10, max_depth=6, n_jobs=1, verbose=0)
-for i in tqdm(range(1000)):
-    clf.fit(X, Y)
 
-predictions = clf.predict(test_data)
+####### svm.SVC ####
+SVC_clf = svm.SVC()
+SVC_clf.fit(X, Y)
+SVC_predictions = SVC_clf.predict(test_data)
+
 
 output_1 = pd.DataFrame({"PassengerId":np.arange(892, 1310),
-                         "Survived":predictions
-                        }).to_csv('predictions.csv', index=False)
+                         "Survived":RF_predictions
+                        }).to_csv('./predictions/RF_predictions.csv', index=False)
+
+output_2 = pd.DataFrame({"PassengerId":np.arange(892, 1310),
+                         "Survived":SVC_predictions
+                        }).to_csv('./predictions/SVC_predictions.csv', index=False)
